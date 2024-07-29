0e317fb537a66f2c3e29c29f664286eaa45663d9
PhucNguyen0001 <phucng228@gmail.com>
2024-07-30 00:41:36 +0700

    start

---
+++ app.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+df = pd.read_csv('bi-cau.csv')
+
+def format_number(val):
+    if isinstance(val, float) and val.is_integer():
+        return str(int(val))
+    else:
+        return str(val)
+
+def search(d_bore=None, d_outside=None, b=None):
+    if not d_bore and not d_outside and not b:
+        return None
+    records = df
+    if d_bore:
+        records = df[df['d bore'] == float(d_bore)]
+
+    if d_outside:
+        records = df[df['D outside'] == float(d_outside)]
+
+    if b:
+        records = df[df['B'] == float(b)]
+
+    return records.to_dict(orient='records')
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['GET'])
+def search1():
+    d_bore = request.args.get('d bore', None)
+    d_outside = request.args.get('D outside', None)
+    b = request.args.get('B', None)
+
+    result = search(d_bore, d_outside, b)
+
+    for record in result:
+        for key, value in record.items():
+            record[key] = format_number(value)
+
+
+    return render_template('search.html', result=result)
+
+
+
+if __name__ == '__main__':
+    # print(search(d_outside=3))
+    app.run()
